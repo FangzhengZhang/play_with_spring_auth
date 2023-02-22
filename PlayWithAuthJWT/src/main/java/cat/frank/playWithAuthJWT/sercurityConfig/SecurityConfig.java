d175447725b0c98c79b6e91b99ba6140ae8344a9
Frank Zhang <fz2907@vt.edu>
2023-02-22 11:44:25 -0500

    Add note on the project and find a way to use antMatcher-like features in the filter. Added in memory UserDetailsService & UserDetails.

--- PlayWithAuthJWT/src/main/java/cat/frank/playWithAuthJWT/sercurityConfig/SecurityConfig.java
+++ PlayWithAuthJWT/src/main/java/cat/frank/playWithAuthJWT/sercurityConfig/SecurityConfig.java
@@ -4,6 +4,10 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 import javax.management.MXBean;
@@ -12,13 +16,57 @@ import javax.management.MXBean;
 public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http.authorizeHttpRequests()
-                //.antMatchers(HttpMethod.GET).permitAll() //this is used to allowed visitor pass
-                .anyRequest()
-                .authenticated()
-                .and()
-                .httpBasic();
+        http.authorizeHttpRequests(authorize -> {
+                    try {
+                        authorize
+                                .requestMatchers("/test/**", "/signup", "/about").permitAll()
+                                .requestMatchers(HttpMethod.GET).permitAll()
+                                .anyRequest().authenticated()
+                                .and()
+                                .httpBasic();
+                    } catch (Exception e) {
+                        throw new RuntimeException(e);
+                    }
+                }
+        );
         return http.build();
 
+    }
+
+    /**
+     * @Bean
+     * SecurityFilterChain web(HttpSecurity http) throws Exception {
+     * 	http
+     * 		// ...
+     * 		.authorizeHttpRequests(authorize -> authorize
+     * 			.requestMatchers("/resources/**", "/signup", "/about").permitAll()
+     * 			.requestMatchers("/admin/**").hasRole("ADMIN")
+     * 			.requestMatchers("/db/**").access(new WebExpressionAuthorizationManager("hasRole('ADMIN') and hasRole('DBA')"))
+     * 			// .requestMatchers("/db/**").access(AuthorizationManagers.allOf(AuthorityAuthorizationManager.hasRole("ADMIN"), AuthorityAuthorizationManager.hasRole("DBA")))
+     * 			.anyRequest().denyAll()
+     * 		);
+     *
+     * 	return http.build();
+     * }
+     */
+
+    /**
+     * Create default admin and user, so in the initial run, we do not need database but still able to use the service.
+     * @return
+     */
+    @Bean
+    public UserDetailsService users(){
+        UserDetails admin = User.builder()
+                .username("admin") // default username
+                .password("password") // default password
+                .roles("ADMIN")
+                .build();
+
+        UserDetails user = User.builder()
+                .username("user") // default username
+                .password("password") // default password
+                .roles("USER")
+                .build();
+        return new InMemoryUserDetailsManager(admin, user);
     }
 }
--- README.md
+++ README.md
@@ -1,11 +1,22 @@
 # play_with_spring_auth
 This project is created for me to play with spring-boot-starter-security and jjwt package. I may use them in my future projects.
 
-Init setup:
+## Init setup:
 - Spring Boot 3.0.1 + Gradle
 - Java 17
 
-Gradle Dependences:
+## Gradle Dependencies:
 - implementation("org.springframework.boot:spring-boot-starter-web")
 - implementation("org.springframework.boot:spring-boot-starter-security")
 - implementation("io.jsonwebtoken:jjwt:0.9.1")
+
+
+## Steps to use Spring Boot Security:
+1. After import Spring Boot Security package to the project, create a folder(securityConfig) 
+    to hold the configuration files. (Make sure you add the @Configuration & @EnableWebSecurity 
+    to the class level.)
+2. In the config class (SecurityConfig), you will create a function with @Bean on it, type is 
+   public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{}
+3. Create few user information in the configuration files, so in the initial run, we can use
+    service without connect to database. (add @Bean to the creation method).
+4. 
